2a44b064107aa3550b8a06d5a6d70588e2153237
Alan Chen <alanchen256@gmail.com>
2023-05-27 11:47:04 -0700

    tutorial stuff

--- src/Message.tsx
+++ src/Message.tsx
@@ -2,7 +2,7 @@
 function Message(){
     const name = 'kms';
     if (name)
-        return <h1>AAAAAAA{name}</h1>;
+        return <h1>AAAAAA{name}</h1>;
     return <h1>bkjnsger</h1>
 }
 
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,33 @@
-function ListGroup(){
-    return <h1>ls</h1>;
+import { useState } from "react";
+
+function ListGroup() {
+  let items = ["test", "test1", "test2"];
+  //   let selectedIndex = -1;
+  //Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      {items.length === 0 && <p>No Items Found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
