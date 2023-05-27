e6063f280cf90a4f44610fca5e957b83834848cf
m-tsoi <mjtsoi@uci.edu>
2023-05-27 11:47:21 -0700

    deleted more files
    vtd deleted

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,40 @@
-function ListGroup(){
-    return <h1>ls</h1>;
+import { Fragment } from "react";
+import { MouseEvent } from "react";
+import { useState } from "react";
+
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading }: ListGroupProps) {
+  const getMessage = () => {
+    return items.length === 0 && <p>nothing</p>;
+  };
+
+  //Event Handler
+  const handleClick = (event: MouseEvent) => console.log(event);
+
+  //Hook (react Built in features)
+  // arr[0] = varaibles
+  // arr[1] = updater function
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [name, setName] = useState("");
+
+  return (
+    <Fragment>
+      <h1>{heading}</h1>
+      {getMessage()}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </Fragment>
+  );
 }
 
 export default ListGroup;
